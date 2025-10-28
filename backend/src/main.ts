81a953de071524e271b06338aafa51006e904e2c
abel <camerasabel56@gmail.com>
2025-10-27 18:58:51 -0600

    coreccion del backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://tu-frontend.vercel.app'],
+    origin: ['http://localhost:3000', 'https://minlike-2.vercel.app'],
   });
 
   await app.listen(process.env.PORT || 3001, '0.0.0.0');
